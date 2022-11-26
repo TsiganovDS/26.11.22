bb49e74fa82de8cd777e8f0e8043806617ae28b1
Даниил Полянский <dm.tsiganov@gmail.com>
2022-11-26 23:30:50 +0700

    34

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿Console.Write("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+ArrayRandomNumbers(numbers);
+Console.Write("Массив: ");
+PrintArray(numbers);
+int count = 0;
+
+for (int z = 0; z < numbers.Length; z++)
+if (numbers[z] % 2 == 0)
+count++;
+
+Console.WriteLine($"Количество чётных чисел в массиве - {count}");
+
+void ArrayRandomNumbers(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+    numbers[i] = new Random().Next(100,1000);
+     
+}    
+
+   void PrintArray(int[] numbers)
+{
+    Console.Write("[");
+    for(int i = 0; i < numbers.Length; i++)
+    {   
+        Console.Write(numbers[i]);
+        if (i < numbers.Length - 1)
+        {
+        Console.Write(", ");
+        }    
+                
+    }
+    Console.WriteLine("]");
+}
+
+
+     
+ 
+
+    
+
